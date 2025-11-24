48cbce05f58810813f7c15255b3d41989b803867
Vitoreism <vitoreism@gmail.com>
2025-11-24 12:12:53 -0300

    refactor: clean up insertion sort code

--- sorting/insertion_sort.py
+++ sorting/insertion_sort.py
@@ -16,50 +16,6 @@ def insertion_sort(a: List[Union[int, float]], n: int) -> List[Union[int, float]
 
 
 
-
-
-
-
-
-
-
-
-def insertion_sort2(a: List[Union[int, float]], n: int) -> List[Union[int, float]]:
-
-    for i in range(1, n):
-
-        pivot = a[i]
-        j = i - 1
-
-        while j>= 0 and a[j] > pivot:
-            a[j+1] = a[j]
-            j -= 1
-        
-        a[j+1] = pivot 
-    
-    return a 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 if __name__ == '__main__':
     array = [1, 5, 2, 10, 2, 3, 4, 7, 9]
-    print(insertion_sort2(a=array, n=len(array)))
+    print(insertion_sort(a=array, n=len(array)))
